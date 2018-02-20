5e042283d945c622d0cf9690650b5242176a81dc
RuiLi92 <rui.lee92@gmail.com>
2018-02-19 22:59:55 -0500

    update.

--- sync.sh
+++ sync.sh
@@ -1,3 +1,7 @@
-ssh dogtail@slave1 " pkill -f edgeServer; cd edgeServer; pwd; rm -rf src; exit;"
-scp -r src dogtail@slave1:~/edgeServer/;
-#$ssh dogtail@slave1 "cd edgeServer; sh compile.sh;"
+git add .
+git commit -am "update."
+git push
+ssh dogtail@slave1 "pkill -f edgeServer; cd edgeServer; git pull; sh compile.sh; exit;"
+ssh dogtail@slave2 "pkill -f edgeServer; cd edgeServer; git pull; sh compile.sh; exit;"
+ssh dogtail@slave3 "pkill -f edgeServer; cd edgeServer; git pull; sh compile.sh; exit;"
+ssh dogtail@master "pkill -f edgeServer; cd edgeServer; git pull; sh compile.sh; exit;"
