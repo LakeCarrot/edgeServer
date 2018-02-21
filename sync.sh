bf4c2d25884212f18b8a2aaaad6922195724e8a2
RuiLi92 <rui.lee92@gmail.com>
2018-02-20 20:26:25 -0500

    update.

--- sync.sh
+++ sync.sh
@@ -2,4 +2,4 @@ git add .
 git commit -am "update."
 git push
 
-ssh dogtail@slave1 "cd edgeServer; git pull; sh compile.sh; exit;"
+ssh dogtail@slave1 "cd edgeServer; pkill -f edgeServer; git pull; sh compile.sh; exit;"
